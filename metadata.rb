bc9e35f404b5242c9057c233d0e27f348e2089ce
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-12-28 11:43:53 +0300

    Add apt and build-essential cookbooks

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures flashcards-cookbook'
 long_description 'Installs/Configures flashcards-cookbook'
 version          '0.1.0'
+
+
+depends 'apt', '~> 2.9.2'
+depends 'build-essential', '~> 2.2.4'
