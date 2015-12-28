9247e018a4a805504e732abc861c27887f715ec7
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2015-12-28 10:53:03 +0300

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'flashcards-cookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures flashcards-cookbook'
+long_description 'Installs/Configures flashcards-cookbook'
+version          '0.1.0'
