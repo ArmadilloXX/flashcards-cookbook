c9d40e5f176917ccfb8c343c1846b1df34f84198
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2016-01-27 10:32:20 +0300

    Add nginx for Kibana. Remove kibana_service recipe

--- recipes/kibana.rb
+++ recipes/kibana.rb
@@ -1,3 +1,35 @@
 include_recipe 'simple-kibana::user'
 include_recipe 'simple-kibana::install'
 include_recipe 'simple-kibana::configure'
+include_recipe 'nginx'
+
+########################################
+# KIBANA SERVICE
+########################################
+
+template "/etc/systemd/system/kibana.service" do
+  source "kibana.service.erb"
+  owner node["kibana"]["user"]
+  group node["kibana"]["group"]
+end
+
+service "kibana" do
+  provider Chef::Provider::Service::Systemd
+  supports start: true, restart: true, stop: true, status: true
+  action [:enable, :start]
+end
+
+########################################
+# NGINX
+########################################
+
+template "#{node['nginx']['dir']}/sites-available/kibana-flashcards" do
+  source "nginx.kibana.erb"
+  owner node['nginx']['user']
+  group node['nginx']['user']
+  mode 0644
+end
+
+nginx_site 'kibana-flashcards' do
+  enable true
+end
