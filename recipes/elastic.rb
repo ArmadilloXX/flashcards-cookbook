6d395e9927079bae6cb0f9b423123a9250d1ca2d
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2016-02-24 10:01:16 +0300

    Replace single quotes to double

--- recipes/elastic.rb
+++ recipes/elastic.rb
@@ -1,20 +1,20 @@
-include_recipe 'java'
-include_recipe 'chef-sugar'
-include_recipe 'elasticsearch'
+include_recipe "java"
+include_recipe "chef-sugar"
+include_recipe "elasticsearch"
 
-elasticsearch_user 'elasticsearch'
-elasticsearch_install 'elasticsearch' do
-  type node['elasticsearch']['install_type'].to_sym
+elasticsearch_user "elasticsearch"
+elasticsearch_install "elasticsearch" do
+  type node["elasticsearch"]["install_type"].to_sym
 end
-elasticsearch_configure 'elasticsearch' do
+elasticsearch_configure "elasticsearch" do
       configuration ({
-      'network.bind_host' => 0,
-      'network.host' => '0.0.0.0',
-      'script.inline' => 'sandbox',
-      'script.aggs' => 'on',
-      
+      "network.bind_host" => 0,
+      "network.host" => "0.0.0.0",
+      "script.inline" => "sandbox",
+      "script.aggs" => "on",
+
     })
 end
-elasticsearch_service 'elasticsearch' do 
+elasticsearch_service "elasticsearch" do
   action :start
 end
