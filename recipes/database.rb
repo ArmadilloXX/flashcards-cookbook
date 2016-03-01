60bddf5df0c96d5546920e9eadb3d3186a768698
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2016-03-01 14:17:56 +0300

    Switch to privileges for db user instead of creating superuser

--- recipes/database.rb
+++ recipes/database.rb
@@ -1,12 +1,3 @@
-#
-# Cookbook Name:: flashcards-cookbook
-# Recipe:: database
-#
-# Copyright (C) 2015 YOUR_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
-
 include_recipe "flashcards-cookbook::general"
 include_recipe "postgresql::server"
 include_recipe "database::postgresql"
@@ -22,22 +13,17 @@ postgresql_connection_info = {
 # Create application database user
 postgresql_database_user node["application"]["database"]["username"] do
   connection postgresql_connection_info
-  superuser true # TODO Grant privileges instead of create superuser
   password node["application"]["database"]["password"]
   action :create
 end
 
-# Add default db config file
+# Grant privileges to  application database user
+postgresql_database_user node["application"]["database"]["username"] do
+  connection    postgresql_connection_info
+  database_name "#{default['application']['name']}_#{default['application']['environment']}"
+  privileges    [:all]
+  action        :grant
+end
 
-# Prepare databases
-# bash "preparation" do
-#   cwd node["webapp"]["directory"]
-#   user node["webapp"]["username"]
-#   code <<-CMDS
-#     rake db:create
-#     rake db:migrate
-#     rake db:seed
-#   CMDS
-# end
 
 
