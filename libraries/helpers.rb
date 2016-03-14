a37873a194a10afa3627598471894216b03c5086
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2016-03-14 13:01:45 +0300

    DRYing attributes setting helpers

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -2,9 +2,15 @@ def files_changed?(repository, path_to_previous_revision, files)
   `cd #{repository} && git log HEAD...$(cat #{path_to_previous_revision}) -- #{files} | wc -l`.to_i > 0
 end
 
-def set_attrs_from_data_bag(databag, item)
-  attributes = get_data_bag_item(databag, item)
-  attributes.to_hash.each do |key, value|
+def set_item_attributes_from_data_bags(data_bags, item)
+  data_bags.each do |data_bag|
+    attributes = get_data_bag_item(data_bag, item).to_hash
+    set_defaults(attributes, item)
+  end
+end
+
+def set_defaults(attributes, item)
+  attributes.each do |key, value|
     node.default["#{item}"][key] =
       case value
       when Hash
@@ -15,13 +21,11 @@ def set_attrs_from_data_bag(databag, item)
   end
 end
 
-def get_data_bag_item(databag, item)
-  case ChefVault::Item.data_bag_item_type("#{databag}", "#{item}")
-  when :normal
-    data_bag_item(databag, item)
-  when :encrypted
-    data_bag_item(databag, item)
+def get_data_bag_item(data_bag, item)
+  case ChefVault::Item.data_bag_item_type(data_bag, item)
+  when :normal, :encrypted
+    data_bag_item(data_bag, item)
   when :vault
-    chef_vault_item("credentials", "#{item}")
+    chef_vault_item(data_bag, item)
   end
 end
