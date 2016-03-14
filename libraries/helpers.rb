9fdb924d144e6bb11c6fd956e44d235dc04601c1
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2016-03-14 11:18:39 +0300

    Add helpers methods

---
+++ libraries/helpers.rb
@@ -0,0 +1,27 @@
+def files_changed?(repository, path_to_previous_revision, files)
+  `cd #{repository} && git log HEAD...$(cat #{path_to_previous_revision}) -- #{files} | wc -l`.to_i > 0
+end
+
+def set_attrs_from_data_bag(databag, item, secret=nil)
+  attributes = get_data_bag_item(databag, item, secret)
+  attributes.to_hash.each do |key, value|
+    node.default["#{item}"][key] =
+      case value
+      when Hash
+        Chef::Mixin::DeepMerge.merge(node["#{item}"][key], value)
+      else
+        value
+      end
+  end
+end
+
+def get_data_bag_item(databag, item, secret=nil)
+  case ChefVault::Item.data_bag_item_type("#{databag}", "#{item}")
+  when :normal
+    data_bag_item(databag, item)
+  when :encrypted
+    data_bag_item(databag, item, secret)
+  when :vault
+    chef_vault_item("credentials", "#{item}")
+  end
+end
