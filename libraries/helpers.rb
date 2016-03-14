88b821352f38de397ad8ba100e0227d9a9c38e30
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2016-03-14 12:37:43 +0300

    Remove unnessessary args from methods

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -2,8 +2,8 @@ def files_changed?(repository, path_to_previous_revision, files)
   `cd #{repository} && git log HEAD...$(cat #{path_to_previous_revision}) -- #{files} | wc -l`.to_i > 0
 end
 
-def set_attrs_from_data_bag(databag, item, secret=nil)
-  attributes = get_data_bag_item(databag, item, secret)
+def set_attrs_from_data_bag(databag, item)
+  attributes = get_data_bag_item(databag, item)
   attributes.to_hash.each do |key, value|
     node.default["#{item}"][key] =
       case value
@@ -15,12 +15,12 @@ def set_attrs_from_data_bag(databag, item, secret=nil)
   end
 end
 
-def get_data_bag_item(databag, item, secret=nil)
+def get_data_bag_item(databag, item)
   case ChefVault::Item.data_bag_item_type("#{databag}", "#{item}")
   when :normal
     data_bag_item(databag, item)
   when :encrypted
-    data_bag_item(databag, item, secret)
+    data_bag_item(databag, item)
   when :vault
     chef_vault_item("credentials", "#{item}")
   end
