df9b4a934553376da85e6993b7458f8a01f17813
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2016-03-19 16:01:33 +0300

    Uncomment Shield plugin install. Test user creation

--- recipes/elastic.rb
+++ recipes/elastic.rb
@@ -5,15 +5,16 @@ include_recipe "elasticsearch"
 elasticsearch_user "elasticsearch"
 elasticsearch_install "elasticsearch" do
   type node["elasticsearch"]["install_type"].to_sym
+  dir  node["elasticsearch"]["base_dir"]
 end
 
-# elasticsearch_plugin 'license' do
-#   action :install
-# end
+elasticsearch_plugin 'license' do
+  action :install
+end
 
-# elasticsearch_plugin 'shield' do
-#   action :install
-# end
+elasticsearch_plugin 'shield' do
+  action :install
+end
 
 elasticsearch_configure "elasticsearch" do
       configuration ({
@@ -23,6 +24,14 @@ elasticsearch_configure "elasticsearch" do
       "script.aggs" => "on"
     })
 end
+
+bash 'create users' do
+  cwd node["elasticsearch"]["base_dir"]
+  code <<-EOH
+    bin/shield/esusers useradd es_admin -r admin -p testpass
+  EOH
+end
+
 elasticsearch_service "elasticsearch" do
   action :start
 end
