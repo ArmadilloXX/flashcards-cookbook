33c80ddf779545ad9ddf5b491a3bd3a8e7fe8234
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2016-04-06 15:03:52 +0300

    Remove PG packages

--- recipes/general.rb
+++ recipes/general.rb
@@ -1,6 +1,6 @@
 include_recipe "build-essential"
 
-%w(postgresql-libs postgresql-devel openssl-devel libyaml-devel libffi-devel
+%w(openssl-devel libyaml-devel libffi-devel
    readline-devel zlib-devel gdbm-devel ncurses-devel
    policycoreutils policycoreutils-python).each do |package_name|
   package package_name do
