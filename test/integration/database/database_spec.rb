e49aabfa2a1eb9417a6b22320f7dbbe9b914359e
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2016-04-06 15:04:31 +0300

    Add test cases

--- test/integration/database/database_spec.rb
+++ test/integration/database/database_spec.rb
@@ -5,3 +5,11 @@ end
 describe service "postgresql-9.4" do
   it { should be_running }
 end
+
+describe command('psql -U postgres -l') do
+  its("stdout") { should match "flashcards_development"}
+end
+
+describe command("psql -U postgres -c \'\\du\'") do
+  its("stdout") { should match "vagrant" }
+end
