85206b5016e99a25a17198b7c8663b013846b369
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2016-04-13 10:35:03 +0300

    Switch to single config data bag item

--- recipes/redis.rb
+++ recipes/redis.rb
@@ -1,3 +1,7 @@
+include_recipe "chef-vault"
+override_settings_from_data_bag('flashcards', 'flashcards_config')
+override_settings_from_data_bag('flashcards', 'flashcards_secrets')
+
 include_recipe "flashcards-cookbook::general"
 include_recipe "redisio"
 include_recipe "redisio::enable"
