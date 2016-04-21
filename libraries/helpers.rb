eee824ad254d1df5ac1eecc1f02c8e2ab00f5eac
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2016-04-21 21:21:36 +0300

    Delete unused methods

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -16,25 +16,6 @@ def override_settings_from_data_bag(data_bag, item)
   end
 end
 
-def set_default_attributes_from_data_bag(data_bag, items)
-  items.each do |item|
-    attributes = get_data_bag_item(data_bag, item).to_hash
-    set_defaults(attributes, data_bag)
-  end
-end
-
-def set_defaults(attributes, main_key)
-  attributes.each do |key, value|
-    node.default[main_key][key] =
-      case value
-      when Hash
-        Chef::Mixin::DeepMerge.merge(node[main_key][key], value)
-      else
-        value
-      end
-  end
-end
-
 def get_data_bag_item(data_bag, item)
   case ChefVault::Item.data_bag_item_type(data_bag, item)
   when :normal, :encrypted
@@ -49,9 +30,4 @@ def set_additional_pg_hba_records
   pg_hba = node['postgresql']['pg_hba'].dup
   new_records.each { |record| pg_hba << record }
   node.default["postgresql"]["pg_hba"] = pg_hba
-end
-
-def database_users
-  item = data_bag_item("postgresql", "database_users")
-  item["db_users"]
 end
